05f85c57d16f0df577695d22cc1e0013707c9478
Diven <1205838869@qq.com>
2022-09-13 09:24:43 +0800

    更新readme

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,14 +1,14 @@
 /*
  * @Date: 2022-09-05 16:52:04
  * @LastEditors: YeKe
- * @LastEditTime: 2022-09-06 17:20:10
+ * @LastEditTime: 2022-09-13 09:24:34
  * @FilePath: \vue3-vite-uniapp\src\config\app.ts
  */
 export const APP_NAME = 'vue3-vite-unapp'
 
-export const PCULIC_URL = 'http://192.168.2.120:3001' // 静态资源的cos地址
+export const PCULIC_URL = 'http://loacalhost:3001' // 静态资源的cos地址
 
-export const HTTP_REQUEST_URL = 'http://192.168.2.120:3001/api'
+export const HTTP_REQUEST_URL = 'http://loacalhost:3001/api'
 
 export const HEADER = {
   'content-type': 'application/json',
